5097fb318a72bd344c114a3169d80c8e62ca9563
Illia <ikuchko@gmail.com>
2016-03-02 10:54:41 -0800

    create category-tile component to show up categories

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: {
-    return this.store.findRecord('category')
+  model() {
+    return this.store.findAll('category')
   }
 });
