5d5dbe2590e64d589ff84884581ea2690ae34c4c
Illia <ikuchko@gmail.com>
2016-03-02 10:07:19 -0800

    create component for new junks

--- app/components/junk-new.js
+++ app/components/junk-new.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      var params = {
+        title: this.get('title'),
+        description: this.get('description'),
+        image: this.get('image')
+      }
+      this.set('title', '');
+      this.set('description', '');
+      this.set('image', '');
+      this.sendAction('save', params);
+    }
+  }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    save(params) {
+      var newJunk = this.store.createRecord('junk', params);
+      newJunk.save()
+    }
+  }
 });
