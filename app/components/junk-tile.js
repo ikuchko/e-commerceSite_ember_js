210f06bd213c494e1b18c84e680f80a11495f444
Illia <ikuchko@gmail.com>
2016-03-03 09:42:06 -0800

    add pannel of item description

--- app/components/junk-tile.js
+++ app/components/junk-tile.js
@@ -2,11 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isTitleShowed: false,
+  isDescriptionShowed: false,
+
   mouseEnter() {
     this.set('isTitleShowed', true);
   },
 
   mouseLeave() {
     this.set('isTitleShowed', false);
+  },
+
+  actions: {
+    openDescription() {
+      this.set('isDescriptionShowed', true);
+    },
+
+    closePanel() {
+      this.set('isDescriptionShowed', false);
+    },
   }
 });
