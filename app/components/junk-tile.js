31d145c362f6e35f9462ef600c96202d903caa49
Illia <ikuchko@gmail.com>
2016-03-02 21:44:15 -0800

    add style to category and junk tiles

--- app/components/junk-tile.js
+++ app/components/junk-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isTitleShowed: false,
+  mouseEnter() {
+    this.set('isTitleShowed', true);
+  },
+
+  mouseLeave() {
+    this.set('isTitleShowed', false);
+  }
 });
